57d22614516df8446a8a14a839e36629d78f4ef6
juliog922 <juliogalindo922@gmail.com>
2024-05-13 22:44:19 +0200

    Established successful connection with backend using Rocket in Docker. Implemented seamless API call from frontend form, marking completion of Lesson 9 in the Udemy Rust frontend with WebAssembly and Yew course.

--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -1,8 +1,24 @@
 use web_sys::HtmlInputElement;
+use gloo_net::Error;
 use gloo_console::log;
-use yew::prelude::*;
+use yew::{prelude::*, platform::spawn_local};
 
-use crate::components::input::*;
+use crate::components::{
+    input::Input,
+    alert::Alert};
+use crate::api::user::{
+    api_login,
+    api_me,
+    MeResponse,
+    LoginResponse
+};
+
+async fn login(username: String, password: String) -> Result<(LoginResponse, MeResponse), Error> {
+    let login_response = api_login(username, password).await?;
+    let me_response = api_me(&login_response.token).await?;
+
+    Ok((login_response, me_response))
+}
 
 #[function_component(LoginForm)]
 pub fn login_form() -> Html {
@@ -11,6 +27,9 @@ pub fn login_form() -> Html {
 
     let password_handle = use_state(String::default);
     let password = (*password_handle).clone();
+
+    let error_message_handle = use_state(String::default);
+    let error_message = (*error_message_handle).clone();
 
     let username_changed = Callback::from(move |e: Event| {
         let target = e.target_dyn_into::<HtmlInputElement>();
@@ -28,19 +47,28 @@ pub fn login_form() -> Html {
 
     let cloned_username = username.clone();
     let cloned_password = password.clone();
+
     let onsubmit = Callback::from(move |e: SubmitEvent| {
         e.prevent_default();
 
         let cloned_username = cloned_username.clone();
         let cloned_password = cloned_password.clone();
+        let cloned_error_handle = error_message_handle.clone();
 
-        log!("Submiting form");
-        log!(cloned_username.clone());
-        log!(cloned_password.clone());
+        spawn_local(async move{
+            match login(cloned_username.clone(), cloned_password.clone()).await {
+                Ok(responses) => log!(responses.1.username),
+                Err(e) => cloned_error_handle.set(e.to_string()),
+            }
+        });
+        
 
     });
     html! {
         <form onsubmit={onsubmit}>
+            if error_message.len() > 0 {
+                <Alert alert_type={"danger"} message={error_message}/>
+            }
             <div class="mb-3">
                 <Input 
                     input_type="text" 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use yew::prelude::*;
 
+mod api;
 mod pages;
 mod components;
 
